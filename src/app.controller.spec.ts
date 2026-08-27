bed2a04ac466530408036ace33600f9e600c733a
Ahad Hossain <ahadxx99@gmail.com>
2026-08-28 02:51:28 +0600

    cicd tesing

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('returns the liveness string', () => {
+      expect(appController.getHello()).toBe('Biye kora lagbe api is running!');
     });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,7 +20,7 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Biye kora lagbe api is running!');
   });
 
   afterEach(async () => {
